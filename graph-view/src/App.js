521c2f3acca4382ce7debdca7fe0150ba9b85047
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2023-10-25 20:58:09 -0300

    dev - Ajustes iniciais para melhor visualização.

--- graph-view/src/App.js
+++ graph-view/src/App.js
@@ -1,10 +1,20 @@
 
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Graph from './components/Graph';
+import GraphControlPanel from './components/GraphControlPanel';
+import * as d3 from 'd3';
 
 const App = () => {
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
+  const [visualParams, setVisualParams] = useState({
+    nodeSize: 5,
+    linkDistance: 50,
+    chargeStrength: -10,
+    collisionRadius: 10,
+    weightThreshold: 2,
+    centerForce: 50,
+  });
   const width = 800;
   const height = 600;
 
@@ -13,31 +23,45 @@ const App = () => {
     if (file) {
       const reader = new FileReader();
       reader.onload = (e) => {
-        const data = JSON.parse(e.target.result);
-        const processedNodes = data.graph.vertices.map(vertex => ({
-          id: vertex.id,
-          label: vertex.label
+        const graph = JSON.parse(e.target.result);
+        const processedNodes = graph.nodes.map(node => ({
+          id: node.id,
+          label: node.label
         }));
   
-        const processedLinks = data.graph.edges.map(edge => ({
-          source: edge.source,
-          target: edge.target,
-          weight: edge.weight
+        const processedLinks = graph.links.map(link => ({
+          source: link.source,
+          target: link.target,
+          weight: link.weight
         }));
-
+  
+        console.log('Processed nodes: ', processedNodes); // Verifica os nós processados
+        console.log('Processed links: ', processedLinks); // Verifica os links processados
+  
         setNodes(processedNodes);
         setLinks(processedLinks);
+        
+        const maxLinkWeight = d3.max(processedLinks, link => link.weight);
+        setVisualParams(prevParams => ({ ...prevParams, maxLinkWeight }));
       };
       reader.readAsText(file);
     }
   };
+
+  const handleParamChange = useCallback((paramName, paramValue) => {
+    setVisualParams(prevParams => ({
+      ...prevParams,
+      [paramName]: paramValue,
+    }));
+  }, []);
 
   return (
     <div className="graph-container">
       <h1>Network Graph</h1>
       <input type="file" onChange={handleFileChange} accept=".json" />
       <div>
-        <Graph nodes={nodes} links={links} width={width} height={height} />
+        <Graph nodes={nodes} links={links} width={width} height={height} visualParams={visualParams} />
+        <GraphControlPanel onParamChange={handleParamChange} visualParams={visualParams} maxWeight={visualParams.maxLinkWeight} />
       </div>
     </div>
   );
--- graph-view/src/components/Graph.js
+++ graph-view/src/components/Graph.js
@@ -2,54 +2,92 @@ import React, { useRef, useEffect } from 'react';
 import './Graph.css';
 import * as d3 from 'd3';
 
-const Graph = ({ nodes, links, width, height }) => {
+const Graph = ({ nodes, links, width, height, visualParams }) => {
   const svgRef = useRef();
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
 
+    // Filtrar links baseados no Weight Threshold antes de renderizar
+    const filteredLinks = links.filter(link => link.weight >= visualParams.weightThreshold);
+
     const simulation = d3.forceSimulation(nodes)
       .alphaDecay(0.01)
-      .force("link", d3.forceLink(links).id(d => d.id).distance(50))
-      .force("charge", d3.forceManyBody().strength(-10))
+      .force("link", d3.forceLink(filteredLinks).id(d => d.id).distance(visualParams.linkDistance))
+      .force("charge", d3.forceManyBody().strength(visualParams.chargeStrength))
       .force("center", d3.forceCenter(width / 2, height / 2))
-      .force("collision", d3.forceCollide().radius(10));
+      .force("collision", d3.forceCollide().radius(visualParams.collisionRadius));
 
-    // Criação dos nós e arestas usando D3.js
-    const link = svg
-      .selectAll(".link")
-      .data(links)
-      .enter()
-      .append("line")
-      .attr("class", "link");
+    svg.selectAll('*').remove();
 
-    const node = svg
-      .selectAll(".node")
-      .data(nodes)
-      .enter()
-      .append("circle")
-      .attr("class", "node")
-      .attr("r", 5);
+	// Adiciona um grupo para todos os elementos e aplica o zoom a este grupo.
+	const g = svg.append("g")
+		.attr("class", "everything");
+	
+	// Criação de elementos gráficos dentro do grupo...
+	const link = g.append("g")
+		.attr("class", "links")
+		.selectAll("line")
+		.data(filteredLinks)
+		.enter()
+		.append("line")
+		.attr("class", "link");
+    
+    // Calcula a quantidade de links para cada nó usando os links filtrados
+    let linkCount = {};
+    filteredLinks.forEach(link => {
+      linkCount[link.source.id] = (linkCount[link.source.id] || 0) + 1;
+      linkCount[link.target.id] = (linkCount[link.target.id] || 0) + 1;
+    });
+  
+    // Mapeia essa quantidade para um tamanho de nó desejado e use visualParams.nodeSize como multiplicador
+    const nodeSizeScale = d3.scaleLinear()
+      .domain([d3.min(Object.values(linkCount))-1, d3.max(Object.values(linkCount))])
+      .range([visualParams.nodeSize, 3 * visualParams.nodeSize]);
+  
+	const node = g.append("g")
+		.attr("class", "nodes")
+		.selectAll("circle")
+		.data(nodes)
+		.enter()
+		.append("circle")
+		.attr("class", "node")
+    .attr("r", d => nodeSizeScale(linkCount[d.id] || 0));
+	
+    const nodeLabels = g.append("g")
+		.attr("class", "node-labels")
+		.selectAll("text")
+		.data(nodes)
+		.enter()
+		.append("text")
+		.attr("x", d => d.x)
+		.attr("y", d => d.y)
+		.text(d => d.label)
+		.attr("dy", "-1em")
+    .attr("fill", "#ecf0f1")  // Cor do texto
+    .style("font-size", "10px");  // Tamanho do texto
+	
+    const linkLabels = g.append("g")
+		.attr("class", "link-labels")
+		.selectAll("text")
+		.data(filteredLinks)
+		.enter()
+		.append("text")
+		.attr("x", d => (d.source.x + d.target.x) / 2)
+		.attr("y", d => (d.source.y + d.target.y) / 2)
+		.text(d => d.weight.toString())
+		.attr("dy", "-0.5em")
+    .attr("fill", "#ecf0f1")  // Cor do texto
+    .style("font-size", "10px");  // Tamanho do texto
+    
+    // Adiciona comportamento de zoom ao SVG.
+    const zoom = d3.zoom()
+      .scaleExtent([0.1, 4])
+      .on("zoom", (event) => {
+        g.attr("transform", event.transform); // Aplica o zoom ao grupo principal.
+      });
 
-    // Adicionando labels aos vértices
-    svg.selectAll(".node-label")
-        .data(nodes)
-        .join("text")
-        .attr("class", "node-label")
-        .attr("x", d => d.x)
-        .attr("y", d => d.y)
-        .text(d => d.label)
-        .attr("dy", "-1em");
-
-    // Adicionando labels às arestas
-    svg.selectAll(".link-label")
-        .data(links)
-        .join("text")
-        .attr("class", "link-label")
-        .attr("x", d => (d.source.x + d.target.x) / 2)
-        .attr("y", d => (d.source.y + d.target.y) / 2)
-        .text(d => d.weight.toString())
-        .attr("dy", "-0.5em");
+    svg.call(zoom);
     
     // Funções de arrastar
     const dragStart = (event, d) => {
@@ -89,17 +127,17 @@ const Graph = ({ nodes, links, width, height }) => {
           .attr("cx", d => d.x)
           .attr("cy", d => d.y);
           
-        // Atualize a posição dos rótulos dos nós
-        svg.selectAll(".node-label")
+        // Atualizando a posição dos rótulos dos nós
+        nodeLabels
           .attr("x", d => d.x)
           .attr("y", d => d.y);
-        
-        // Atualize a posição dos rótulos das arestas
-        svg.selectAll(".link-label")
+    
+        // Atualizando a posição dos rótulos das arestas
+        linkLabels
           .attr("x", d => (d.source.x + d.target.x) / 2)
           .attr("y", d => (d.source.y + d.target.y) / 2);
     });
-  }, [nodes, links, width, height]);
+  }, [nodes, links, width, height, visualParams]);
 
   return (
     <svg ref={svgRef} width={width} height={height}></svg>
